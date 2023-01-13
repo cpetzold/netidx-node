47fa0ec8e384625e8ba35a64b7215e4c10447b0e
Eric Stokes <eric@architect.gg>
2023-01-13 14:44:04 -0500

    add CRs

--- src/subscriber.rs
+++ src/subscriber.rs
@@ -37,15 +37,29 @@ pub struct Subscriber {
 impl Subscriber {
     #[napi]
     pub fn subscribe(&mut self, path: String) {
+        // CR estokes: why do you clone the path string when you're
+        // passed an owned string?  cloning a String is a new
+        // allocation, cloning a path is not, so build a path and
+        // clone that as many times as needed.
+        // CR estokes: Remove the type annotation, it is implied by Path::from
+        // CR estokes: just call p path, since you never use path again.
         let p: Path = Path::from(path.clone());
         let val = self.subscriber.durable_subscribe(p.clone());
         val.updates(UpdatesFlags::empty(), self.updates.clone());
+        // CR estokes: woooooooooah, hang on. I think you may be
+        // confused about what Arc::make_mut actually does. It is not
+        // a way to share mutable data between threads, you need a
+        // Mutex for that. If you don't have the ONLY pointer to the
+        // Arc then make_mut will copy the entire table and make a new
+        // Arc that you can have mutable access to. The other holders
+        // will not see your updates.
         Arc::make_mut(&mut self.dvals).insert(val.id(), (p.clone(), val.clone()));
         self.path_to_subid.insert(p, val.id());
     }
 
     #[napi]
     pub fn unsubscribe(&mut self, path: String) {
+        // CR estokes: all the same things I said about this in subscribe
         let p: Path = Path::from(path.clone());
         if let Some(id) = self.path_to_subid.get(&p) {
             Arc::make_mut(&mut self.dvals).remove(id);
@@ -55,6 +69,8 @@ impl Subscriber {
 }
 
 async fn subscriber_task(mut from_sub: mpsc::Receiver<BatchedUpdate>, callback: NCb) {
+    // CR estokes: Since you are only looking at one async thing you don't need to
+    // use select! here. Just use while let Some(batch) = from_sub.next().await ...
     loop {
         select! {
             batch = from_sub.select_next_some() => {
@@ -73,7 +89,8 @@ pub enum EventType {
 
 #[napi(object)]
 pub struct Event {
-    pub path: Option<String>,
+    // CR estokes: I exposed the u64 in SubId, change this to u64
+    pub path: Option<String>, 
     pub value: Option<JsUnknown>,
     pub kind: EventType,
 }
