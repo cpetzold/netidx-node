aa3ac9d1fd998daa18b3a7e026d8052307039e6a
Conner Petzold <cpetzold@gmail.com>
2023-01-13 22:34:13 -0700

    Fix subscriber issues

--- example/subscriber.ts
+++ example/subscriber.ts
@@ -1,9 +1,106 @@
-import { createSubscriber } from "..";
+import { EventEmitter } from "stream";
+import {
+  createSubscriber,
+  Duration,
+  F32,
+  F64,
+  I32,
+  I64,
+  U32,
+  U64,
+  V32,
+  V64,
+  Z32,
+  Z64,
+  Subscriber as SubscriberInner,
+  EventType,
+} from "..";
+
+type Value =
+  | null
+  | boolean
+  | string
+  | number
+  | U32
+  | V32
+  | Z32
+  | I32
+  | U64
+  | V64
+  | Z64
+  | I64
+  | F32
+  | F64
+  | Duration
+  | Date
+  | Buffer
+  | Value[];
+
+type SubscribeEvent = {
+  path: string;
+  type: EventType;
+  value?: Value;
+};
+
+class Subscriber extends EventEmitter {
+  idToPath: Map<bigint, string>;
+  pathToId: Map<string, bigint>;
+  inner: SubscriberInner;
+
+  constructor() {
+    super();
+
+    this.idToPath = new Map();
+    this.pathToId = new Map();
+
+    this.inner = createSubscriber((batch) => {
+      batch.forEach(({ id, type, value }) => {
+        const path = this.idToPath.get(id)!;
+        const event: SubscribeEvent = { type, value: value as Value, path };
+        if (path) {
+          this.emit(path, event);
+        }
+        this.emit("any", event);
+      });
+    });
+
+    this.on("newListener", (path) => {
+      const id = this.inner.subscribe(path);
+      this.idToPath.set(id, path);
+      this.pathToId.set(path, id);
+    });
+
+    this.on("removeListener", (path) => {
+      const id = this.pathToId.get(path);
+      if (this.listenerCount(path) === 0 && id) {
+        this.inner.unsubscribe(id);
+      }
+    });
+  }
+
+  any(callback: (event: SubscribeEvent) => void) {
+    this.on("any", callback);
+    return () => this.off("any", callback);
+  }
+
+  subscribe(path: string, callback: (event: SubscribeEvent) => void) {
+    this.on(path, callback);
+    return () => this.off(path, callback);
+  }
+}
 
 async function run() {
-  const subscriber = createSubscriber((batch) => console.log("!", batch));
+  const subscriber = new Subscriber();
 
-  subscriber.subscribe("/hello/world");
+  subscriber.subscribe("/", ({ path, type, value }) => {
+    if (type === EventType.Unsubscribed) {
+      console.log(`Unsubscribed|${path}`);
+    } else {
+      console.log(`${path}|${typeof value}|${value}`);
+    }
+  });
+
+  subscriber.any(console.log);
 
   while (true) {
     await new Promise((resolve) => setTimeout(resolve, 1000));
--- src/subscriber.rs
+++ src/subscriber.rs
@@ -1,7 +1,5 @@
-use std::sync::Arc;
-
 use crate::util::js_of_value;
-use futures::{channel::mpsc, prelude::*, select};
+use futures::{channel::mpsc, prelude::*};
 use fxhash::FxHashMap;
 use napi::{
     bindgen_prelude::*,
@@ -29,54 +27,33 @@ type NCb = ThreadsafeFunction<BatchedUpdate, ErrorStrategy::Fatal>;
 pub struct Subscriber {
     subscriber: NSubscriber,
     updates: mpsc::Sender<BatchedUpdate>,
-    dvals: Arc<FxHashMap<SubId, (Path, Dval)>>,
-    path_to_subid: FxHashMap<Path, SubId>,
+    dvals: FxHashMap<u64, Dval>,
 }
 
 #[napi]
 impl Subscriber {
     #[napi]
-    pub fn subscribe(&mut self, path: String) {
-        // CR estokes: why do you clone the path string when you're
-        // passed an owned string?  cloning a String is a new
-        // allocation, cloning a path is not, so build a path and
-        // clone that as many times as needed.
-        // CR estokes: Remove the type annotation, it is implied by Path::from
-        // CR estokes: just call p path, since you never use path again.
-        let p: Path = Path::from(path.clone());
-        let val = self.subscriber.durable_subscribe(p.clone());
+    pub fn subscribe(&mut self, path: String) -> u64 {
+        let path = Path::from(path);
+        let val = self.subscriber.durable_subscribe(path.clone());
         val.updates(UpdatesFlags::empty(), self.updates.clone());
-        // CR estokes: woooooooooah, hang on. I think you may be
-        // confused about what Arc::make_mut actually does. It is not
-        // a way to share mutable data between threads, you need a
-        // Mutex for that. If you don't have the ONLY pointer to the
-        // Arc then make_mut will copy the entire table and make a new
-        // Arc that you can have mutable access to. The other holders
-        // will not see your updates.
-        Arc::make_mut(&mut self.dvals).insert(val.id(), (p.clone(), val.clone()));
-        self.path_to_subid.insert(p, val.id());
+
+        let id = val.id().inner();
+        self.dvals.insert(id, val.clone());
+        val.id().inner()
     }
 
     #[napi]
-    pub fn unsubscribe(&mut self, path: String) {
-        // CR estokes: all the same things I said about this in subscribe
-        let p: Path = Path::from(path.clone());
-        if let Some(id) = self.path_to_subid.get(&p) {
-            Arc::make_mut(&mut self.dvals).remove(id);
-            self.path_to_subid.remove(&p);
-        }
+    pub fn unsubscribe(&mut self, sub_id: BigInt) {
+        let (_, id, _) = sub_id.get_u64();
+        self.dvals.remove(&id);
     }
 }
 
 async fn subscriber_task(mut from_sub: mpsc::Receiver<BatchedUpdate>, callback: NCb) {
-    // CR estokes: Since you are only looking at one async thing you don't need to
-    // use select! here. Just use while let Some(batch) = from_sub.next().await ...
     loop {
-        select! {
-            batch = from_sub.select_next_some() => {
-                callback.call(batch, ThreadsafeFunctionCallMode::Blocking);
-            },
-            complete => break,
+        if let Some(batch) = from_sub.next().await {
+            callback.call(batch, ThreadsafeFunctionCallMode::Blocking);
         }
     }
 }
@@ -89,16 +66,14 @@ pub enum EventType {
 
 #[napi(object)]
 pub struct Event {
-    // CR estokes: I exposed the u64 in SubId, change this to u64
-    pub path: Option<String>, 
+    pub id: BigInt,
     pub value: Option<JsUnknown>,
+    #[napi(js_name = "type")]
     pub kind: EventType,
 }
 
-#[napi]
+#[napi(ts_args_type = "callback: (batch: Event[]) => void")]
 pub fn create_subscriber(callback: JsFunction) -> Result<Subscriber> {
-    let dvals: Arc<FxHashMap<SubId, (Path, Dval)>> = Arc::new(FxHashMap::default());
-    let idvals = dvals.clone();
     let tsfn: NCb = callback.create_threadsafe_function(
         0,
         move |mut ctx: ThreadSafeCallContext<BatchedUpdate>| {
@@ -106,13 +81,14 @@ pub fn create_subscriber(callback: JsFunction) -> Result<Subscriber> {
                 .value
                 .drain(..)
                 .map(|(id, event)| {
-                    let path = idvals.get(&id).map(|(path, _)| path.clone().to_string());
                     Ok(match event {
-                        NEvent::Unsubscribed => {
-                            Event { path, kind: EventType::Unsubscribed, value: None }
-                        }
+                        NEvent::Unsubscribed => Event {
+                            id: id.inner().into(),
+                            kind: EventType::Unsubscribed,
+                            value: None,
+                        },
                         NEvent::Update(v) => Event {
-                            path,
+                            id: id.inner().into(),
                             kind: EventType::Update,
                             value: Some(js_of_value(&mut ctx.env, &v.clone())?),
                         },
@@ -129,7 +105,6 @@ pub fn create_subscriber(callback: JsFunction) -> Result<Subscriber> {
         subscriber: NSubscriber::new(cfg, DesiredAuth::Local)
             .map_err(|_| Error::from_reason("Couldn't create subscriber"))?,
         updates: updates_tx,
-        dvals: dvals.clone(),
-        path_to_subid: FxHashMap::default(),
+        dvals: FxHashMap::default(),
     })
 }
